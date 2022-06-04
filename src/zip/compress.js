b68863ffd68caad5f883d9bd100e61a929af04bc
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-04 20:57:09 +0300

    feat: compress.js and decompress.js are done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,16 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const gzip = createGzip();
+  const source = createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+  const destination = createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+  await pipeline(source, gzip, destination);
 };
+
+compress();
