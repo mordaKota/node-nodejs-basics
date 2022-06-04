e09de3d0c26573bbba14f48bbb5f8a8a6705d7ba
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-04 13:59:31 +0300

    feat: copy.js is done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,29 @@
+import { mkdir, readdir, copyFile, access } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const copy = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const isFolderExists = async (folder) => {
+    try {
+      console.log(__dirname);
+      await access(path.join(__dirname, folder));
+    } catch {
+      return false;
+    }
+    return true;
+  }
+
+  if (await isFolderExists('files-copy')) {
+    console.log ("FS operation failed");
+  } else {
+    await mkdir(path.join(__dirname, 'files-copy'));
+    const fileNames = await readdir(path.join(__dirname, 'files'));
+    await Promise.all(
+      fileNames.map(n => copyFile(path.join(path.join(__dirname, 'files'), n), path.join(path.join(__dirname, 'files-copy'), n)))
+    );
+  }
 };
+
+copy();
