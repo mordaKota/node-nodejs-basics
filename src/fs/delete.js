b0ede5efa42bc0b9cfb140cf8cb8fdcbbe5a5df6
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-04 14:48:14 +0300

    feat: delete.js is done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,26 @@
+import { rm, access } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 export const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename); 
+
+  const isFileExists = async (filename) => {
+    try {
+      await access(path.join(__dirname, 'files', filename));
+    } catch {
+      return false;
+    }
+    return true;
+  }
+
+  if (!await isFileExists ('fileToRemove.txt')) {
+    console.log("FS operation failed")
+  } else {
+    await rm(path.join(__dirname, 'files', 'fileToRemove.txt'))
+  }
 };
+
+remove();
