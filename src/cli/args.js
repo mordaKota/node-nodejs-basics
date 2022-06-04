de24b59920a5e2bf6b5de2232e4ba7d6b424a94a
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-04 17:27:55 +0300

    feat: args.js is done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
+//Example: node .\src\cli\args.js --cat purr --dog bark
+
 export const parseArgs = () => {
-    // Write your code here 
+  let output = [];
+  const arrOfArgs = process.argv.slice(2); 
+  for (let i = 0; i < arrOfArgs.length; i++) {
+    if (arrOfArgs[i].indexOf('--') === 0) {
+      output.push(arrOfArgs[i].substring(2, arrOfArgs[i].length) + " is " + arrOfArgs[i + 1]);
+    }
+  }
+  console.log(output);
 };
+
+parseArgs();
