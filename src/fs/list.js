73e4146eff26466b624a52fd6ef402056eaa5c5a
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-04 14:57:10 +0300

    feat: list.js is done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,26 @@
+import {readdir, access } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const isFolderExists = async (folder) => {
+    try {
+      console.log(__dirname);
+      await access(path.join(__dirname, folder));
+    } catch {
+      return false;
+    }
+    return true;
+  }
+
+  if (!await isFolderExists('files')) {
+    console.log ("FS operation failed");
+  } else {
+    const fileNames = await readdir(path.join(__dirname, 'files'));
+    console.log(fileNames);
+  }
 };
+
+list();
