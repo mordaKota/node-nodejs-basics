b35ec1632615c96e23a0bf0aa82011dfc30a176c
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-04 19:35:17 +0300

    feat: transform.js is done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,19 @@
+import { pipeline, Transform } from 'node:stream';
+
 export const transform = async () => {
-    // Write your code here 
-};
+  const transformStream = new Transform({
+    transform(chunk, enc, callback) {
+      const reversedChunk = chunk.toString().trim().split("").reverse().join('');
+      callback(null, reversedChunk+ '\n');
+    }
+  });
+  
+  pipeline(
+    process.stdin,
+    transformStream,
+    process.stdout,
+    error => { console.log(error);}
+  );
+}
+
+transform();
