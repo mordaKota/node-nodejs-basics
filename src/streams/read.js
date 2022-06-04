d5532c7da7c5fd526aab026418880a153c4145df
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-04 18:21:44 +0300

    feat: stream-read.js is done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,11 @@
+import { createReadStream } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  createReadStream(path.join(__dirname, 'files', 'fileToRead.txt')).pipe(process.stdout)
 };
+
+read();
