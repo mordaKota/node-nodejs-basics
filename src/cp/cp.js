74431dc43da54a0cb9970e6779f56143fb7fc5a2
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-05 20:15:47 +0300

    feat: cp.js is done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,17 @@
+import { spawn } from 'node:child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  
+  const child = spawn('node', [path.join(__dirname, 'files', 'script.js')], { shell: true });
+  
+  process.stdin.on('data', (chunk) => {
+    child.stdin.write(chunk);
+  });
+  child.stdout.on('data', (response) => console.log(`${response}`))
 };
+
+spawnChildProcess();
