4ca6e7c8102eacf3a3b7f8c6743a673d82fe91ee
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-05 20:46:55 +0300

    fix: fix cp.js logic

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,12 +6,16 @@ export const spawnChildProcess = async (args) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   
-  const child = spawn('node', [path.join(__dirname, 'files', 'script.js')], { shell: true });
+  const child = spawn('node', [path.join(__dirname, 'files', 'script.js'), ...args], { shell: true });
   
   process.stdin.on('data', (chunk) => {
     child.stdin.write(chunk);
+    if (chunk.toString().trim() === 'CLOSE') {
+      process.exit(0);
+    }
+    
   });
   child.stdout.on('data', (response) => console.log(`${response}`))
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv.slice(2));
